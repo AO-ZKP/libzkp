141a184cdaec7bb0b9de43dc4505771cfb85523d
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-09-04 23:01:59 +0530

    add updated install scripts for wasm64

--- build-rust.sh
+++ build-rust.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-cargo build --release
+# this is for your local machine's architecture
+# cargo build --release
 
-# The resulting wasm file will be in target/wasm64-unknown-unknown/release/libgroth16_wasm.wasm
+# cp target/release/libgroth16_wasm.a ./bin/
+
+# build command for wasm64
+# Ensure you have the nightly toolchain installed
+rustup toolchain install nightly
+
+# Add the wasm64-unknown-unknown target
+rustup target add wasm64-unknown-unknown --toolchain nightly
+
+## ignore the error after this, the rustup is retarded
+
+cargo +nightly build --target wasm64-unknown-unknown --release -Z build-std=panic_abort,std
 
 # The resulting wasm file will be in target/wasm64-unknown-unknown/release/libgroth16_wasm.a
-cp target/release/libgroth16_wasm.a ./bin/
+cp target/wasm64-unknown-unknown/release/libgroth16_wasm.a ./bin/
+
+
