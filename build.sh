4cd16664e8fee45642f49d8ac92a8a39e144b257
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-09-08 01:49:49 +0530

    add tweaks

--- build.sh
+++ build.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# RUSTFLAGS="--cfg=web_sys_unstable_apis -Z wasm-c-abi=spec" \
-# cargo +nightly build -Zbuild-std=std,panic_unwind,panic_abort --target=wasm64-unknown-unknown --release -Zbuild-std-features=panic_immediate_abort
+RUSTFLAGS="--cfg=web_sys_unstable_apis -Z wasm-c-abi=spec" \
+cargo +nightly build -Zbuild-std=std,panic_unwind,panic_abort --target=wasm64-unknown-unknown --release -Zbuild-std-features=panic_immediate_abort
 
-# #cargo build --target=wasm32-unknown-unknown --release
+#wasm-opt target/wasm64-unknown-unknown/release/groth16_wasm.wasm -O4 -o target/wasm64-unknown-unknown/release/groth16_wasm.wasm
+#cargo build --target=wasm32-unknown-unknown --release
 
-# rm -rf bin include pkg        
-# mkdir -p bin include pkg
+rm -rf bin include pkg
+mkdir -p bin include pkg
 
 
-# rustup run nightly cbindgen  --crate groth16_wasm --output include/groth16_wasm.h # --config cbindgen.toml 
+rustup run nightly cbindgen  --crate groth16_wasm --output include/groth16_wasm.h # --config cbindgen.toml 
 
-# wasm-bindgen target/wasm64-unknown-unknown/release/groth16_wasm.wasm --out-dir ./pkg  --target nodejs
+rustup run nightly wasm-bindgen target/wasm64-unknown-unknown/release/groth16_wasm.wasm --out-dir ./pkg  --target nodejs
 
 ################################ NEW BUILD SCRIPT ################################
 
-# High level command, may break
-RUSTFLAGS="--cfg=web_sys_unstable_apis -Z wasm-c-abi=spec" rustup run nightly wasm-pack build --target nodejs --out-name groth16_wasm -- --target wasm64-unknown-unknown -Z build-std=std,panic_unwind,panic_abort
+# High level command, not compatible,  breaking, actually this works better, but the above is more closer to ao flags
+#RUSTFLAGS="--cfg=web_sys_unstable_apis -Z wasm-c-abi=spec" rustup run nightly wasm-pack build --target nodejs --out-name groth16_wasm -- --target wasm64-unknown-unknown -Z build-std=std,panic_unwind,panic_abort -Z build-std-features=panic_immediate_abort
+
+
+
 
 cp target/wasm64-unknown-unknown/release/*.wasm ./bin
 cp target/wasm64-unknown-unknown/release/*.a ./bin
