e563d884236babd39560351cc3a041630fddd4ed
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-10-12 13:31:21 +0530

    add setup and check enviroment script

---
+++ setup-enviroment.sh
@@ -0,0 +1,13 @@
+echo "Setting up enviroment"
+echo ""
+echo "Checking for rustup:"
+rustup --version
+echo ""
+echo "Checking for cargo:"
+cargo --version
+echo ""
+echo "Adding nightly toolchain:"
+rustup toolchain install nightly
+echo ""
+echo "Adding wasm32-wasip1 target:"
+rustup target add wasm32-wasip1 --toolchain nightly
