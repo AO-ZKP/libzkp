6ea7a51c90b11836281c87fe13dbc7e11c4e7ea0
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-10-12 13:37:37 +0530

    test emscripten

--- setup-enviroment.sh
+++ setup-enviroment.sh
@@ -1,13 +1,21 @@
 echo "Setting up enviroment"
 echo ""
+
 echo "Checking for rustup:"
 rustup --version
 echo ""
+
 echo "Checking for cargo:"
 cargo --version
 echo ""
+
 echo "Adding nightly toolchain:"
 rustup toolchain install nightly
 echo ""
+
 echo "Adding wasm32-wasip1 target:"
 rustup target add wasm32-wasip1 --toolchain nightly
+echo ""
+
+echo "Adding wasm32-unknown-emscripten target:"
+rustup target add wasm32-unknown-emscripten --toolchain nightly
